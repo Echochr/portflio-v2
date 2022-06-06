b3c12c698239460118199b67913df81f4d101a84
Chris Ho <chris.ho@technet-asia.com>
2022-06-06 10:55:24 +0800

    remove unnecessary default boilerplate files and install tailwindcss related dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: {
+    extend: {},
+    screen: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+  },
+  plugins: [],
+};
