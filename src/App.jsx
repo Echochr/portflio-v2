1d637734bd6f012f38fc6702975919ce1e3d55d8
Chris Ho <chris.ho@technet-asia.com>
2022-06-07 10:11:41 +0800

    style(app): remove root level padding for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
-import tw from 'twin.macro';
 
 import Navbar from './components/Navbar';
 
-const AppContainer = styled.div`
-  ${tw`
-    p-6
-    lg:px-32
-    lg:py-12
-  `}
-`;
-
 function App() {
   return (
-    <AppContainer>
+    <>
       <Navbar />
-    </AppContainer>
+    </>
   );
 }
 
