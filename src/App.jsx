dce545d7f0df2a6af25b8813e555252685ae4e1b
Chris Ho <chris.ho@technet-asia.com>
2022-06-09 11:57:28 +0800

    add Spacer to layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Navbar from './components/Navbar';
+import Spacer from './components/Spacer';
 import SkillsExpSection from './components/Skills-Exp';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Spacer />
       <SkillsExpSection />
     </>
   );
