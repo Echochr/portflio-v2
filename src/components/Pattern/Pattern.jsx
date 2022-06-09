2bcf82cb8130e0a228e794d6bc616d95cf6ca683
Chris Ho <chris.ho@technet-asia.com>
2022-06-09 17:01:20 +0800

    feat(pattern): create Pattern component

---
+++ src/components/Pattern/Pattern.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import { motion } from 'framer-motion';
+
+import pattern from '../../assets/pattern/background2.png';
+
+function Pattern() {
+  return (
+    <motion.div
+      style={{
+        position: 'fixed',
+        width: '100vw',
+        top: '-75%',
+        opacity: 0.75,
+        zIndex: '-10',
+      }}
+      animate={{ rotate: 360 }}
+      transition={{ ease: 'linear', duration: 150, repeat: Infinity }}
+    >
+      <img src={pattern} alt="" />
+    </motion.div>
+  );
+}
+
+export default Pattern;
